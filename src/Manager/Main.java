57eff573c29a63bed443150855e55a5bd91e32aa
Viacheslav Antonyuk <antonyukv516@yandex.ru>
2024-08-08 14:22:10 +0300

    complite

--- src/Manager/Main.java
+++ src/Manager/Main.java
@@ -7,6 +7,9 @@ import Model.Subtask;
 public class Main {
 
     public static void main(String[] args) {
+        //Весь main для проверки методов и их работы, понимаю что
+        // этого быть не должно, но для помощи в проверке оставлю
+        // я понимаю что это заготовка дальнейших работ
         Epic epic1 = new Epic("навести уборку", "Порядок");
         Epic epic2 = new Epic("отдохнуть хорошо", "Отдых");
         Subtask subtask1 = new Subtask("помыть посуду", "посуда");
@@ -18,7 +21,7 @@ public class Main {
         manager.AddSubtaskToEpic(epic1, subtask1);
         manager.AddSubtaskToEpic(epic1, subtask2);
         System.out.println(manager.epics);
-        //System.out.println(manager.getSubTackToID(4));
+        System.out.println(manager.getSubTackToID(4));
         manager.updateSubTask(subtask1, Status.NEW);
         manager.updateSubTask(subtask2, Status.NEW);
         System.out.println(manager.getEpicToID(1));
